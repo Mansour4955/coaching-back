b8b8d9cf8832b285d24a1ac11f92c1d3dd1bef38
Amine Mansour <mansour4955amine@gmail.com>
2024-05-07 22:16:07 +0200

    finish a

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get("/api/images/:imageName", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
